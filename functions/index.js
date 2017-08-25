7948f05e5445da740396b39523a3de2d2ae72a0c
artem <ar.arkhipov@gmail.com>
2017-08-25 23:34:43 +0300

    checkImage function added

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
+const vision = require('@google-cloud/vision')();
+const gcs = require('@google-cloud/storage')();
 
 exports.sendHelloEmail = functions.database.ref('/users/{userId}').onCreate(event => {
     const data = event.data.val();
@@ -24,3 +26,25 @@ exports.sendHelloEmail = functions.database.ref('/users/{userId}').onCreate(even
         console.error('Error during sending email:', error);
     });
 });
+
+exports.checkImage = functions.storage.object().onChange(event => {
+    const object = event.data;
+    // Exit if this is a deletion or a deploy event.
+    if (object.resourceState === 'not_exists' || !object.name) return;
+
+    const image = {
+        source: {imageUri: `gs://${object.bucket}/${object.name}`}
+    };
+
+    return vision.safeSearchDetection(image).then(batchAnnotateImagesResponse => {
+        const result = batchAnnotateImagesResponse[0].safeSearchAnnotation;
+
+        if (result.violence === 'LIKELY' || result.violence === 'VERY_LIKELY' || result.violence === 'POSSIBLE') {
+            console.log('ALARM, POSSIBLY VIOLENT IMAGE!');
+            return gcs.bucket(object.bucket).file(object.name).delete();
+        } else {
+            console.log(`Image ${object.name} is OK.`);
+        }
+    });
+});
+
